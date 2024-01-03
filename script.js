d95fa48213f971aa99736187fb822b19c52eb5ab
HusniddinWeb <husniddinshidiyev95@gmail.com>
2024-01-03 09:58:21 +0500

    My portfolio

---
+++ script.js
@@ -0,0 +1,32 @@
+window.addEventListener('DOMContentLoaded', function(){
+    window.addEventListener('scroll', function(){
+        const header = document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 0);
+    })
+
+    const menuBtn = document.querySelector('.menu-btn');
+    const navigation = document.querySelector('.navigation');
+    const navigationItems = document.querySelectorAll('.navigation a');
+
+    menuBtn.addEventListener('click', function() {
+        menuBtn.classList.toggle('active');
+        navigation.classList.toggle('active');
+    })
+
+    navigationItems.forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            menuBtn.classList.remove('active');
+            navigation.classList.remove('active');
+        })
+    })
+
+    const scrollBtn = this.document.querySelector('.scrollTOTop-btn');
+    window.addEventListener('scroll', () => {
+        scrollBtn.classList.toggle('active', window.scrollY > 500)
+    })
+    scrollBtn.addEventListener('click', () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    })
+});
+
